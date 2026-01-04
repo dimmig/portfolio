6a13fb3c3b9a59cf9387cf722a24e0544d1e7b0f
D. Galagan <dima@airtrail.co>
2026-01-04 12:45:14 +0100

    added robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,22 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+            {
+                userAgent: "bingbot",
+                allow: "/",
+                crawlDelay: 0,
+            },
+            {
+                userAgent: "msnbot",
+                allow: "/",
+                crawlDelay: 0,
+            },
+        ],
         sitemap: "https://dimmig.me/sitemap.xml",
     };
 }
